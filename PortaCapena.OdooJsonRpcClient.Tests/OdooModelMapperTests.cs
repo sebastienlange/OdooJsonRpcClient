2b97c02374b8ca38ff97b28618cf91fd0bb66bfb
Sébastien LANGE <slange@greisch.com>
2025-07-25 13:57:25 +0200

    Add CanMap_JsonObjectToString test

--- PortaCapena.OdooJsonRpcClient.Tests/OdooModelMapperTests.cs
+++ PortaCapena.OdooJsonRpcClient.Tests/OdooModelMapperTests.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using FluentAssertions;
-using System.Linq;
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using PortaCapena.OdooJsonRpcClient.Converters;
 using PortaCapena.OdooJsonRpcClient.Models;
 using PortaCapena.OdooJsonRpcClient.Shared.Models;
-using Xunit;
-using PortaCapena.OdooJsonRpcClient.Converters;
-using Newtonsoft.Json;
-using Xunit.Abstractions;
+using System;
 using System.IO;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
 
 namespace PortaCapena.OdooJsonRpcClient.Tests
 {
@@ -63,6 +64,22 @@ namespace PortaCapena.OdooJsonRpcClient.Tests
             var result = (PriorityPurchaseOrderOdooEnum)output;
 
             Assert.Equal(PriorityPurchaseOrderOdooEnum.Urgent, result);
+        }
+
+        [Fact]
+        public void CanMap_JsonObjectToString()
+        {
+            const string json = "{\r\n  \"currency_id\": 1,\r\n  \"currency_pd\": 0.01,\r\n  \"company_currency_id\": 1,\r\n  \"company_currency_pd\": 0.01,\r\n  \"has_tax_groups\": false,\r\n  \"subtotals\": [\r\n    {\r\n      \"tax_groups\": [],\r\n      \"tax_amount_currency\": 0.0,\r\n      \"tax_amount\": 0.0,\r\n      \"base_amount_currency\": 462.5,\r\n      \"base_amount\": 462.5,\r\n      \"name\": \"Montant hors taxes\"\r\n    }\r\n  ],\r\n  \"base_amount_currency\": 462.5,\r\n  \"base_amount\": 462.5,\r\n  \"tax_amount_currency\": 0.0,\r\n  \"tax_amount\": 0.0,\r\n  \"same_tax_base\": true,\r\n  \"total_amount_currency\": 462.5,\r\n  \"total_amount\": 462.5\r\n}";
+
+            var jObject = JObject.Parse(json);
+            Assert.Equal(JTokenType.Object, jObject.Type);
+            OdooModelMapper.ConverOdooPropertyToDotNet(
+                typeof(string),
+                jObject,
+                out var output
+            );
+
+            Assert.Equal(json, output);
         }
     }
 }
